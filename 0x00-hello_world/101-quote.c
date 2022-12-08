12a68331eaea2e769bce2fb81ff343f804b326a2
mwogoi <jinaromwogoi@gmail.com>
2022-12-08 09:49:16 -0800

    Write to std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Prints to std erro
+ * Return: 1 if succesful.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
