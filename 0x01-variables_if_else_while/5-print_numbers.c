d42e6912f1c4b3216818b8e18b3b0faaa8c08379
mwogoi <jinaromwogoi@gmail.com>
2022-12-09 11:16:21 -0800

    Print numbers 1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print single digit numbers below 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%i", a);
+	}
+	putchar('\n');
+	return (0);
+}
