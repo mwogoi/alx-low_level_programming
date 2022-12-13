8d0500427932aa7186d4ab867ef3d15fb4ec8380
mwogoi <jinaromwogoi@gmail.com>
2022-12-13 10:18:59 -0800

    Printing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - prototype Writes _putchar to the output
+ * Description: Prints character using putchar command
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
