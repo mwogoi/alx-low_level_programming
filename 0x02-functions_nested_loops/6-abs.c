57b43f84cc2e9e4c9d643e69bcf3ca754e7c4f32
mwogoi <jinaromwogoi@gmail.com>
2022-12-13 11:48:16 -0800

    Get absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Checks the absolute value of the interger
+ * @numb: The numbers being tested
+ * Return: The absolute number  value
+ */
+int _abs(int numb)
+{
+	if (numb >= 0)
+		return (numb);
+	else
+		return (-numb);
+}
