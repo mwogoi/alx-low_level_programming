395ab8736af13480a09d3967eb8931598e2008ef
mwogoi <jinaromwogoi@gmail.com>
2022-12-13 10:40:44 -0800

    Print using a defined function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - prototype Writes _putchar to the output
+ * main -  Writes _putchar to the output
  * Description: Prints character using putchar command
  * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
