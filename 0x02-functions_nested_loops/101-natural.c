7226d269e5a271b3bf432f94fca6aa3a4b580627
mwogoi <jinaromwogoi@gmail.com>
2022-12-14 10:48:34 -0800

    Natural numbers conditioned

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints natural numbers divisible by 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 1024; number++)
+	{
+		if (((number % 3) != 0) || ((number % 5) != 0))
+			continue;
+		else
+			printf("The number is %d ", number);
+	}
+	_putchar('\n');
+
+	return (0);
+}
