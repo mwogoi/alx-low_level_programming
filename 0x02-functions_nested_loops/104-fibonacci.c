b1a3a23d9acbfbe133a6f26d28148274ce0ac095
mwogoi <jinaromwogoi@gmail.com>
2022-12-14 12:18:35 -0800

    Expanded fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print fibonacci numbers up 98
+ * Return: Always 0
+ */
+int main(void)
+{
+	int counter;
+	unsigned long numb1, numb2, sum;
+
+	numb1 = 0;
+	numb2 = 1;
+
+	for (counter = 0; counter < 98; counter++)
+	{
+		sum = numb1 + numb2;
+		numb1 = numb2;
+		numb2 = sum;
+		if (counter != 97)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
