b98dde4748d90a1e103deeefe95ef4221a1903a5
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 15:53:17 -0800

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 - 9
+ * Returns: nothing
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+		_putchar(c);
+	_putchar('\n');
+}
