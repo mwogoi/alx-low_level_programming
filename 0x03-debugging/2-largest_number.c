91947aea3223ee1e9033631630ac0911faca3abd
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 13:36:12 -0800

    .

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a > b)
 	{
-		if (c < b)
-			largest = a;
-		else if (a > c)
+		if (a < c)
 			largest = a;
 		else
 			largest = c;
 	}
+	else if (b > c)
+		largest = b;
 	else
-	{
-		if (c > b)
-			largest = c;
-		else
-			largest = b;
-	}
+		largest = c;
+
 	return (largest);
 }
