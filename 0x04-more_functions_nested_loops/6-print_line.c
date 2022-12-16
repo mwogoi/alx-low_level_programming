0fde5dbec55fb9eca19797e49bd5e76cb7960404
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 16:46:45 -0800

    Staight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - print a straigt line
+ * @n: length of line
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int counter;
+
+	for (counter = 0; counter <= n; counter++)
+		_putchar('_');
+	_putchar('\n');
+}
