6111ee116b2bf33c166526ae38f41ff51316a4bf
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 16:04:41 -0800

    Exclude 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print numbers btwn 0 and 9 excluding 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int counter;
+
+	for (counter = 48; counter <= 57; counter++)
+	{
+		while (counter != 50 && counter != 52)
+			_putchar(counter);
+	}
+	_putchar('\n');
+}
