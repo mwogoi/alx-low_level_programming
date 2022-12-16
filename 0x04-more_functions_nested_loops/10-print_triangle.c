220e0aaa5f888cd5fff1055f4a586403da62e2c2
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 18:24:47 -0800

    .

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle -print triangle
+ * @size: lines
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b < (size - a); b++)
+			_putchar(' ');
+		for (b--; b < size; b++)
+			_putchar(35);
+		if (a < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
