a03f7b4a7c4bc662409694595d1c391301b9868b
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 18:38:36 -0800

    m

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - largest prime factor
+ * Return: 0
+ */
+int main(void)
+{
+	long int numb, prime;
+
+	numb = 612852475143;
+	for (prime = 2; prime <= numb; prime++)
+	{
+		if (numb % prime == 0)
+		{
+			n /= prime;
+			prime--;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
