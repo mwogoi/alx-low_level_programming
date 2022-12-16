a286aab74a5e1ece34845c0880657cdf2714aabb
mwogoi <jinaromwogoi@gmail.com>
2022-12-16 08:55:38 -0800

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int a, b, c;
+	unsigned int numb;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		a = n * -1;
+		numb = -n;
+		_putchar('-');
 	}
 	else
-		a = n;
-	b = a;
-	c = 1;
-	while (b > 9)
-	{
-		b /= 10;
-		c *= 10;
-	}
-	for (; c >= 1; c /= 10)
-		_putchar(((a / c) % 10) + 48);
+		numb = n;
+	if (numb / 10)
+		print_number(numb / 10);
+	_putchar((numb % 10) + '0');
 }
