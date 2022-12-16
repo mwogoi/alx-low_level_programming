376e9a69edaff1d9b1dda6bf9da0b3626c1cdd9b
mwogoi <jinaromwogoi@gmail.com>
2022-12-16 09:18:33 -0800

    .

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,7 +12,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
-int print_number(int n);
 void print_number(int n);
 
 #endif
