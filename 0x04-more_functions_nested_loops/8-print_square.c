576b0788a24839546d2810d4198b28eb06135295
mwogoi <jinaromwogoi@gmail.com>
2022-12-16 08:10:03 -0800

    Squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,15 @@
 void print_square(int size)
 {
 	int count1, count2;
-
-	for (count1 = 0; count1 < size; count1++)
-	{
-		for (count2 = 0; count2 < size; count2++)
-			_putchar('#');
-
+	if (size <= 0)
 		_putchar('\n');
-
-		if (size <= 0)
+	else
+	{
+		for (count1 = 0; count1 < size; count1++)
+		{
+			for (count2 = 0; count2 < size; count2++)
+				_putchar('#');
 			_putchar('\n');
+		}
 	}
 }
