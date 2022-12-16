e56375b892108b638fe570b7ccb6ce0d4a8ebf7a
mwogoi <jinaromwogoi@gmail.com>
2022-12-15 17:49:12 -0800

    ,

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,18 @@ int main(void)
 	int i;
 
 	i = 1;
-	printf("%d", n);
+	printf("%d", i);
 
-	for (n = 2; n <= 100; n++)
+	for (i = 2; i <= 100; i++)
 	{
-		if ((n % 5 == 0) && (n % 3 == 0))
+		if ((i % 5 == 0) && (i % 3 == 0))
 			printf("FizzBuzz");
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf(" %d", n);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
