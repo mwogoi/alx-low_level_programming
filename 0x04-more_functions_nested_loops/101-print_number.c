3c226cb69ecae5cb0d356a4ea1da8c8b89cc58c9
Mwogoi <50166115+mwogoi@users.noreply.github.com>
2022-12-16 23:06:08 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * print_negative - Print a negative multiple digit number
+ * @n: Number to print
+ * Return: 1 if number is negative, 0 otherwise
+ */
+int print_negative(int n)
+{
+	int max;
+	int zeros;
+
+	zeros = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		for (max = -1000000000; max <= -1; max /= 10)
+		{
+			if (max == -1)
+			{
+				zeros = 1;
+			}
+			if (n > max)
+			{
+				if (zeros)
+					_putchar('0');
+			}
+			else
+			{
+				zeros = 1;
+				_putchar('0' + (n / max));
+				n %= (-max);
+			}
+		}
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * print_number - Print a multiple digit number including negative sign
+ * @n: Number to print
+ */
+void print_number(int n)
+{
+	int max;
+	int zeros;
+
+	zeros = 0;
+	if (!(print_negative(n)))
+	{
+		for (max = 1000000000; max >= 1; max /= 10)
+		{
+			if (max == 1)
+			{
+				zeros = 1;
+			}
+			if (n < max)
+			{
+				if (zeros)
+					_putchar('0');
+			}
+			else
+			{
+				zeros = 1;
+				_putchar('0' + (n / max));
+				n %= max;
+			}
+		}
+	}
+}
