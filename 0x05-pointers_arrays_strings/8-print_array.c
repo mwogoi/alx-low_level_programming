abe776c5b4e68f5cb998f4b5cd2eab5158781267
mwogoi <jinaromwogoi@gmail.com>
2022-12-19 15:05:13 -0800

    Printing array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,11 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
+	{
 		printf("%d", a[i]);
-	if (i < n - 1)
 		printf(", ");
+		if (i == n - 1)
+			printf("\n");
+	}
 	_putchar('\n');
 }
