2ed011421c8195f652bd1ded79af1662d476d752
mwogoi <jinaromwogoi@gmail.com>
2022-12-19 13:59:36 -0800

    Print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,10 @@ void puts2(char *str)
 {
 	int j, i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; (*(str + i) != '\0'); i++)
 	{
-		for (j = 0; j < i; j = j + 2)
-			_putchar(str[j]);
+		if (!(i % 2))
+			_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
-
