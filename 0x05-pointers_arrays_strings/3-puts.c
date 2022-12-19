1928f7808951b3d2c09c6604ef0d5c7dff4e55bd
mwogoi <jinaromwogoi@gmail.com>
2022-12-19 12:20:13 -0800

    Printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: string to be printed
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	if (str[i] != '\0')
+	{
+		_putchar(str[i]);
+			i++;
+	}
+	_putchar('\n');
+}
