df690c9c4b697659ee5d64244e86ffbfef7f50bc
mwogoi <jinaromwogoi@gmail.com>
2022-12-19 15:36:44 -0800

    Copy and paste data

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcpy - copy string pointers
+ * @dest: Destination of string to be copied to
+ * @src: Source of the string
+ * Return: Destinaton site
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+	int i;
+
+	while (src[n] != '\0';)
+		n++;
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	i = 1;
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++
+	}
+	return (dest);
+}
