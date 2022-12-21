c1d7ede11a2cf477d28ed3e8e8d71ce2b10c139f
mwogoi <jinaromwogoi@gmail.com>
2022-12-21 07:56:33 -0800

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int count;
 
-	for (count = 0; s1[count] != '\0' && s2[count] != '\0'; count++)
+	for (count = 0; ((s1[count] != '\0') && (s2[count] != '\0')); count++)
 	{
 		if (s1[count] != s2[count])
 			return (s1[count] - s2[count]);
