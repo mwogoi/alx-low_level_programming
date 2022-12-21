56cb93904ceca1a6bef32611e9af7fb5bf355c28
mwogoi <jinaromwogoi@gmail.com>
2022-12-21 08:37:57 -0800

    Encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,13 +7,12 @@
 char *leet(char *string);
 {
 	int count1, count2;
-	char s1[], s2[];
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	s1[] = "aAeEoOtTlL";
-	s2[] = "4433007711";
 	for (count1 = 0; string[count1] != '\0'; count1++)
 	{
-		for (count2 = 0; count2 <= 9; count2++)
+		for (count2 = 0; count2 < 10; count2++)
 		{
 			if (string[count1] == s1[count2])
 				string[count1] = s2[count2];
