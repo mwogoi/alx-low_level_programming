314a0edadd0fd16dfa35cd0369b760805fe22365
mwogoi <jinaromwogoi@gmail.com>
2022-12-21 04:13:40 -0800

    Adding infinite values

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,17 +11,18 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int count1, count2, count3;
 
-	for (count1 = 0; r[count1] != '\0' && r[count1] <= size_r ; count1++)
+
+	for (count1 = 0; r[count1] != '\0' && r[count1] <= size_r; count1++)
 	{
 		for (count2 = 0; n1[count2] != '\0'; count2++)
 		{
-			for (count3 = 0; n2[count3] != '\0';count3++)
+			for (count3 = 0; n2[count3] != '\0'; count3++)
 			{
 				n1[count2] = n2[count3];
 				count2++;
 			}
 			r[count1] = n1[count2];
-			count1 ++;
+			count1++;
 		}
 		return (r);
 	}
