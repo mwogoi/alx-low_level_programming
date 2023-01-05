5a41b3b637ea817d6ddc1bee846b78bd94680dad
mwogoi <jinaromwogoi@gmail.com>
2023-01-05 11:02:15 -0800

    Prime suspects

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,13 +2,12 @@
 int is_divisible(int num, int div);
 int is_prime_number(int n);
 /**
- * * is_divisible - Checks if a number is divisible.
- * * @num: The number to be checked.
- * * @div: The divisor.
- * *
- * * Return: If the number is divisible - 0.
- * * If the number is not divisible - 1.
- * */
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ * Return: If the number is divisible - 0.
+ * If the number is not divisible - 1.
+ */
 int is_divisible(int num, int div)
 {
 	if (num % div == 0)
@@ -16,4 +15,16 @@ int is_divisible(int num, int div)
 	if (div == num / 2)
 		return (1);
 	return (is_divisible(num, div + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_divisible(n, 2));
 }
