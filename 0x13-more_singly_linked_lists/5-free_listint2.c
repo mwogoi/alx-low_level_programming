30448ebe8c56cafe10b2bbbf3338bbdc8a04945b
mwogoi <jinaromwogoi@gmail.com>
2023-02-03 00:47:53 -0800

    Free a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,7 @@
  * @head : point to the address of the pointer to the head of a list
  * Return : void
  */
-void free_listint2(listint_t **head);
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
