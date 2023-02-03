9a035824e5e2ca1a021729a022bb7de9e56d1628
mwogoi <jinaromwogoi@gmail.com>
2023-02-02 23:50:32 -0800

    Adds a new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /*
  * add_nodeint - Adds a new node at the start of a list
- * @h: pointer to the addres of the first element in the list
+ * @head: pointer to the addres of the first element in the list
  * @n: variable containing the new node value
  * Return: Null if it fails and new node of succesfull
  */
